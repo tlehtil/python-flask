0664724376ad5d3051743e425cf666b333db9421
Teemu Lehtilä <teemleht@csc.fi>
2022-09-21 09:57:00 +0300

    template

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Flask!"
+    user = {'username': 'Teemu'}
+    return render_template('index.html', title="Pullo", user=user)

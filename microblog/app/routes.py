24e821c53f2319084a530a9fa43a276704369418
Teemu Lehtilä <teemleht@csc.fi>
2022-09-21 14:48:47 +0300

    add posts to index

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Teemu'}
-    return render_template('index.html', title="Pullo", user=user)
+    posts = [
+        {
+            'author': {'username':'Väyrysen viskipullo'},
+            'body': 'Aina on hyvä syy röypätä'
+        },
+        {
+            'author': {'username':'Pelkuri'},
+            'body': 'Maksan velat'
+        }
+    ]
+    return render_template('index.html', title="Pullo", user=user, posts=posts)

1faa4d9e8a9f3bb03b8329ae1f65b29ea05115a8
Teemu Lehtilä <teemleht@csc.fi>
2022-10-06 18:24:51 +0300

    subtemplate for posts, profile editing page

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -17,6 +17,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<ID: {}> <User {}>'.format(self.id,self.username)

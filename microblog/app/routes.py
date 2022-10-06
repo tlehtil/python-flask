1679234edff5abdcba81e045e075f8ab8c763cd9
Teemu Lehtilä <teemleht@csc.fi>
2022-10-06 17:49:23 +0300

    avatars, user profile page, login required to view profile

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,10 +1,10 @@
-from flask import render_template, flash, redirect, url_for
 from app import app
+from urllib import request
+from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm
 from app.models import User
-from flask_login import current_user, login_user
-from app.models import User
-from flask_login import logout_user
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
@@ -20,6 +20,7 @@ def index():
         }
     ]
     return render_template('index.html', title="Pullo", posts=posts)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -31,9 +32,23 @@ def login():
             flash('Invalid username or password!')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)

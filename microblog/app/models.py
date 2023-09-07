959093a43e95b23d30f37243b4c65973c9a0cbac
Teemu Lehtilä <teemleht@csc.fi>
2023-09-07 15:54:47 +0300

    Add followers table to db

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -11,6 +11,9 @@ from hashlib import md5
 def load_user(id):
     return User.query.get(int(id))
 
+followers = db.Table('followers', db.Column('follower_id', db.Integer, db.ForeignKey('user.id')), 
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -19,6 +22,9 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship('User', secondary=followers, primaryjoin=(followers.c.follower_id == id),
+                               secondartjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<ID: {}> <User {}>'.format(self.id,self.username)
@@ -41,3 +47,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+    

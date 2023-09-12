acceb4474387d436e7245caa43450a59a35dd674
tlehtil <teemu.lehtila@tutanota.com>
2023-09-12 17:03:35 +0300

    korjattu typo, migraatio kantaan

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -23,7 +23,7 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     followed = db.relationship('User', secondary=followers, primaryjoin=(followers.c.follower_id == id),
-                               secondartjoin=(followers.c.followed_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
                                backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):

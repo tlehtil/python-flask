727be6a935c109a613ffce32ca92435ded7404a6
Teemu Lehtilä <teemleht@csc.fi>
2023-09-14 16:24:46 +0300

    added flask-moment

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from logging.handlers import RotatingFileHandler
 import os
 
@@ -15,6 +16,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
+moment = moment(app)
 
 from app import routes, models, errors
 

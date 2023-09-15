b3a5d6dc7aac3b95b6eca5d27e864fd60c4b3dfd
Teemu Lehtilä <teemleht@csc.fi>
2023-09-15 17:24:47 +0300

    add Flask-Babel and start i18n

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,11 +1,12 @@
 import logging
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
 from logging.handlers import RotatingFileHandler
 import os
 
@@ -17,8 +18,13 @@ login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 from app import routes, models, errors
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 if not app.debug:
     if not os.path.exists('logs'):

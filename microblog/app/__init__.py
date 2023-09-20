87ebd78add2313c88adc31ac44cd1fdaed9ecf4a
tlehtil <teemu@lehti.la>
2023-09-20 19:42:03 +0300

    Errors blueprint migration

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -22,7 +22,10 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 babel = Babel(app)
 
-from app import routes, models, errors
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
+from app import routes, models
 
 @babel.localeselector
 def get_locale():
